bfd435a67a0e765cd3f5cad5b3a66def8c7eef0b
Sammie-oje <omokhojesamuel8@gmail.com>
2026-05-04 20:55:17 +0100

    Light mode neomorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import Button from "./components/Button.jsx";
 function App() {
     return (
         <div className="space-x-2">
+           <Button>Submit</Button>
             <Button variant={"primary"}>Submit</Button>
             <Button variant={"destructive"}>Cancel</Button>
+            <Button variant={"warning"}>Warning</Button>
         </div>
     );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,38 +1,50 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import { useRender } from "@base-ui/react/use-render";
 import { mergeProps } from "@base-ui/react/merge-props";
-import {twMerge} from "tailwind-merge";
+import { twMerge } from "tailwind-merge";
 
 const buttonVariants = cva(
-    "inline-flex justify-center items-center bg-red-200",
+    "inline-flex justify-center items-center cursor-pointer hover:opacity-90 focus-visible:ring-foreground focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none  border-3 border-foreground border-background shadow-raised bg-linear-to-tl disabled:opacity-50 disabled:pointer-events-none font-medium active:shadow-inset transition-all duration-250 ease-out active:scale-97",
     {
         variants: {
             variant: {
-                default: "",
-                primary: "bg-blue-400",
-                destructive: "bg-red-400",
-                warning: ""
+                default: "from-background to-highlight/75",
+                primary: "text-primary from-primary/5 to-primary/25",
+                destructive:
+                    "text-destructive from-destructive/5 to-destructive/25",
+                warning: "text-warning from-warning/5 to-warning/25"
             },
             size: {
                 default: "h-12 px-4",
                 icon: "h-12 w-12"
+            },
+            shape: {
+                default: "rounded-xl",
+                circle: "rounded-full"
             }
         },
         defaultVariants: {
             variant: "default ",
-            size: "default"
+            size: "default",
+            shape: "default"
         }
     }
 );
 
-interface ButtonProps  
-   extends useRender.ComponentProps<"button">,VariantProps<typeof buttonVariants> {}
+interface ButtonProps
+    extends
+        useRender.ComponentProps<"button">,
+        VariantProps<typeof buttonVariants> {}
 
 export default function Button(props: ButtonProps) {
     const mergedProps = mergeProps(props, {
         className: twMerge(
-          buttonVariants({ variant: props.variant, size: props.size }),
-          ),
+            buttonVariants({
+                variant: props.variant,
+                size: props.size,
+                shape: props.shape
+            })
+        )
     });
 
     const element = useRender({
