c599ba999cf15af8606f053e468555a54b1ed5c7
Sammie-oje <omokhojesamuel8@gmail.com>
2026-05-05 05:42:32 +0100

    Implemented dark mode & toggling theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import Button from "./components/Button.jsx";
-
+import Button from "./components/Button.tsx";
+import Nav from "./components/Nav.tsx";
 
 function App() {
     return (
+     
         <div className="space-x-2">
-           <Button>Submit</Button>
+           <Nav/>
+            <Button>Submit</Button>
             <Button variant={"primary"}>Submit</Button>
             <Button variant={"destructive"}>Cancel</Button>
             <Button variant={"warning"}>Warning</Button>
+            <Button variant={"primary"} disabled>
+                Warning
+            </Button>
         </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "./providers/theme-provider.tsx";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <ThemeProvider defaultTheme="dark" storage-theme="vite-ui-theme">
+            <App />
+        </ThemeProvider>
+    </StrictMode>
+);
