97b2103e7ad1486324df3a6d707b9fb6159c7da8
Sammie-oje <omokhojesamuel8@gmail.com>
2026-06-16 16:11:29 +0100

    Created a music player feature using the library

--- src/App.tsx
+++ src/App.tsx
@@ -14,98 +14,96 @@ import {
     CardFooter
 } from "./components/Card.tsx";
 import { TabsRoot, TabsList, TabsTab, TabsPanel } from "./components/Tabs.tsx";
+import { SkipBack, SkipForward, Play, Pause } from "lucide-react";
+import { useState } from "react";
 
 function App() {
+    const [isPlaying, setIsPlaying] = useState(false);
     return (
-        <>
-            <div className="space-x-2">
-                <Nav />
-                <Button>Submit</Button>
-                <Button variant={"primary"}>Submit</Button>
-                <Button variant={"destructive"}>Cancel</Button>
-                <Button variant={"warning"}>Warning</Button>
-                <Button variant={"primary"} disabled>
-                    Warning
-                </Button>
-            </div>
+        <main>
+            <Nav />
+            <section className="max-w-4xl px-8 py-12 mx-auto text-center">
+                <h1 className="md:text-3xl text-xl font-semibold tracking-tight">
+                    Beautiful design with neumorphic design
+                </h1>
+                <p className="mt-1 md:text-lg text-sm text-muted-foreground">
+                    Crafted with care and attention to detail. Built on top of
+                    Base UI with Tailwind CSS. Accessible, customizable and
+                    ready to use in your projects.
+                </p>
+            </section>
+            <section>
+                <div className="flex-1 space-y-6">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Music Player</CardTitle>
+                            <CardDescription>
+                                Enjoy high quality music streaming
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="space-y-8 flex flex-col justify-center items-center">
+                                <img
+                                    className={`w-56 h-56 shadow-raised rounded-full border-4 border-border ${isPlaying ? "animate-spin" : ""}`}
+                                    alt="track-cover"
+                                    src="./assets/vite.svg"
+                                />
+                                <div className="text-center space-y-2">
+                                    <CardTitle>Feel good inc.</CardTitle>
+                                    <CardDescription>Gorillaz</CardDescription>
+                                </div>
+                                <div>
+                                    <NeuProgress
+                                        className="min-w-xs"
+                                        max={100}
+                                        value={45}
+                                    />
+                                    <div className="flex justify-between mt-2 text-xs text-muted-foreground">
+                                        <span>0:00</span>
+                                        <span>3:32</span>
+                                    </div>
+                                </div>
 
-            <div className="m-8">
-                <Card>
-                    <CardHeader>
-                        <CardTitle>This is my title</CardTitle>
-                        <CardDescription>
-                            This is the price of my product
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <ul>
-                            <li>This is one benefit</li>
-                            <li>This is one benefit</li>
-                            <li>This is one benefit</li>
-                        </ul>
-                    </CardContent>
-                    <CardFooter>
-                        <NeuSlider variant={"secondary"} />
-                    </CardFooter>
-                </Card>
-
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Progress bars</CardTitle>
-                        <CardDescription>
-                            Simple vertical/horizontal bars
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="space-y-4 flex h-36 gap-4">
-                        <NeuProgress
-                            orientation={"vertical"}
-                            max={100}
-                            value={25}
-                        />
-                        <NeuProgress
-                            orientation={"vertical"}
-                            max={100}
-                            value={50}
-                        />
-                        <NeuProgress
-                            orientation={"vertical"}
-                            max={100}
-                            value={75}
-                        />
-                    </CardContent>
-                    <CardContent className="space-y-4 ">
-                        <NeuProgress
-                            variant={"secondary"}
-                            max={100}
-                            value={25}
-                        />
-                        <NeuProgress max={100} value={50} />
-                        <NeuProgress max={100} value={75} />
-                    </CardContent>
-                    <CardFooter>
-                        <Input></Input>
-                    </CardFooter>
-                </Card>
-
-                <Card>
-                    <CardHeader>
-                        <TabsRoot>
-                            <TabsList className={"space-x-4"}>
-                                <TabsTab value={"overview"}>Overview</TabsTab>
-                                <TabsTab value={"user"}>User</TabsTab>
-                            </TabsList>
-                            <TabsPanel value={"overview"}>
-                                <h2>This is overview</h2>
-                                <p>This is more info about the overview</p>
-                            </TabsPanel>
-                            <TabsPanel value={"user"}>
-                                <h2>This is for the user</h2>
-                            </TabsPanel>
-                        </TabsRoot>
-                    </CardHeader>
-                </Card>
-            </div>
-        </>
+                                <div className="flex justify-center items-center gap-4">
+                                    <Button
+                                        aria-label="Previous Track"
+                                        size={"icon"}
+                                        shape={"circle"}
+                                        variant={"primary"}
+                                    >
+                                        <SkipBack className="h-4 w-4" />
+                                    </Button>
+                                    <Button
+                                        aria-label={
+                                            isPlaying ? "Pause" : "Play"
+                                        }
+                                        className="w-20 h-12"
+                                        size={"icon"}
+                                        shape={"circle"}
+                                        variant={"primary"}
+                                    >
+                                        {isPlaying ? (
+                                            <Pause className="h-4 w-4" />
+                                        ) : (
+                                            <Play className="h-4 w-4" />
+                                        )}
+                                    </Button>
+                                    <Button
+                                        aria-label="Next Track"
+                                        size={"icon"}
+                                        shape={"circle"}
+                                        variant={"primary"}
+                                    >
+                                        <SkipForward className="h-4 w-4" />
+                                    </Button>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className="flex-1 space-y-6"></div>
+            </section>
+        </main>
     );
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,6 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import { useRender } from "@base-ui/react/use-render";
 import { mergeProps } from "@base-ui/react/merge-props";
-import { twMerge } from "tailwind-merge";
 
 const buttonVariants = cva(
     "inline-flex justify-center items-center cursor-pointer hover:opacity-90 focus-visible:ring-foreground focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none  border-3 border-border shadow-raised bg-linear-to-tl disabled:opacity-50 disabled:pointer-events-none font-medium active:shadow-inset transition-all duration-250 ease-out active:scale-97",
@@ -38,13 +37,12 @@ interface ButtonProps
 
 export default function Button(props: ButtonProps) {
     const mergedProps = mergeProps(props, {
-        className: twMerge(
-            buttonVariants({
-                variant: props.variant,
-                size: props.size,
-                shape: props.shape
-            })
-        )
+        className: buttonVariants({
+            variant: props.variant,
+            size: props.size,
+            shape: props.shape
+        }),
+        tabIndex: 0
     });
 
     const element = useRender({
