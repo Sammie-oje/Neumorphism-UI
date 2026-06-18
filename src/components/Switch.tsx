2165fb334247adfcb3fa5184a7ee1498e0befc7f
Sammie-oje <omokhojesamuel8@gmail.com>
2026-06-18 06:28:40 -0700

    Added a cookie banner card

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -4,7 +4,7 @@ import { useRender } from "@base-ui/react/use-render";
 import { mergeProps } from "@base-ui/react/merge-props";
 
 const switchVariants = cva(
-    "flex items-center cursor-pointer h-5.5 w-12 rounded-full focus-visible:outline-none focus-visible:ring-offset-2 focus-visible:ring-foreground focus-visible:ring-2 bg-background shadow-inset data-[checked]:bg-primary border-1 border-highlight data-[checked]:border-primary "
+    "flex items-center cursor-pointer h-5.5 w-12 rounded-full focus-visible:outline-none focus-visible:ring-offset-2 focus-visible:ring-foreground focus-visible:ring-2 bg-background shadow-inset data-[checked]:bg-primary border-1 border-highlight data-[checked]:border-primary disabled:opacity-50 "
 );
 const thumbVariants = cva(
     "h-5 w-5 bg-background relative rounded-full block ease-out transition-all duration-350 border-1 border-highlight after:w-1 after:h-1 after:absolute after:top-1/2 after:left-1/2 after:-translate-1/2 after:bg-muted-foreground after:rounded-full shadow-inset translate-x-0 data-[checked]:translate-x-6.5 data-[checked]:after:bg-primary"
